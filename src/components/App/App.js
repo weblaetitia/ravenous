a4dcffe62294150dc88bd4f7dd1a9f161b2128cf
Laetitia Langlois <weblaetitia@hotmail.com>
2020-09-17 17:34:07 +0200

    pass restaurants information from app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,39 @@ import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BusinessList';
 import './App.css';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businessTwo = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'Prego Pizzeria',
+  address: '2359 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 6.5,
+  reviewCount: 30
+}
+
+const businesses = [business, businessTwo, business, businessTwo, business, businessTwo]
+
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
